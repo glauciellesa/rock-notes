c828ad91151702409ca506c1f6e53fb49f6d0402
Glaucielle Sá <glauciellesa@gmail.com>
2024-03-28 17:22:53 +0100

    add search component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-
 import ListNotes from '@components/listNotes/ListNotes.vue'
-
+import SearchBar from '@components/searchBar/SearchBar.vue'
 </script>
 
 <template>
 
   <div class="flex gap-8 flex-col sm:flex-row w-full">
-    <div class="w-1/5 border-r-2 p-4">
+    <div class="w-full sm:w-1/5 sm:border-r-2 p-4">
       <ListNotes />
     </div>
     <main class="flex-1">
+      <div class="flex justify-between pb-3 sm:pt-3">
+        <SearchBar />
+      </div>
       <RouterView />
     </main>
   </div>
---
+++ src/components/searchBar/SearchBar.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/solid';
+
+</script>
+
+<template>
+  <div class="flex gap-4 border-2 w-fit rounded p-1">
+    <span>
+      <MagnifyingGlassIcon class="h-6 w-6 text-gray-400" />
+    </span>
+    <input type="text" placeholder="Search" class="outline-none">
+  </div>
+</template>
