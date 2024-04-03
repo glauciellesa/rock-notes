63231d309f6a971787efe4e4c4fadcb544fd330f
Glaucielle Sá <glauciellesa@gmail.com>
2024-04-03 21:19:26 +0200

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
 import Header from '@components/header/Header.vue'
+import ToolBar from '@components/toolBar/ToolBar.vue'
+
+import { useAuthStore } from "@store/auth"
+import { computed } from 'vue';
+
+const authStore = useAuthStore();
+
+const isLogged = computed(() => authStore.isUserLoggedIn);
 </script>
 
 <template>
-  <Header />
+  <div class="shadow-md">
+    <Header />
+    <ToolBar v-if="isLogged" />
+  </div>
   <RouterView />
 </template>
--- src/components/notes/Notes.vue
+++ src/components/notes/Notes.vue
@@ -1,51 +1,26 @@
 <script setup lang="ts">
 import ListNotes from '@components/listNotes/ListNotes.vue'
-
+import CardNotes from '@components/cardNotes/CardNotes.vue'
 import { useNotesStore } from '@store/notes';
-import { useRouter } from 'vue-router';
-import { PlusCircleIcon, TrashIcon, ListBulletIcon, Squares2X2Icon } from '@heroicons/vue/24/solid';
+import { computed } from 'vue';
 
 const store = useNotesStore();
-const router = useRouter()
 
-const newNote = async () => {
-  const id = await store.newNote()
-  router.push(`/notes/${id}`)
-}
+const isDisplayList = computed(() => store.getSelectedDisplayMode == 'list');
+
+
 </script>
 
 <template>
-  <div class="px-6 flex gap-4 flex-col sm:flex-row w-full">
-    <div class="w-full sm:w-1/5 sm:border-r-2 p-4">
-      <div class="pb-8">
-        <button @click="store.deleteNote" class="flex justify-end items-center w-full">
-          <div class="flex items-center gap-2">
-            <!--            <button
-              class="focus:bg-gray-100 focus:rounded focus:shadow-md hover:bg-gray-200 hover:rounded hover:shadow-md p-1">
-              <ListBulletIcon class="h-6 w-6" />
-            </button>
-            <button
-              class="focus:bg-gray-100 focus:rounded focus:shadow-md hover:bg-gray-200 hover:rounded hover:shadow-md p-1">
-              <Squares2X2Icon class="h-6 w-6" />
-            </button>-->
-          </div>
-          <span>
-            <TrashIcon class="h-6 w-6" />
-          </span>
-        </button>
-      </div>
+  <div class=" px-6 flex gap-4 flex-col sm:flex-row w-full" v-if="isDisplayList">
+    <div class="w-full md:w-1/3 sm:border-r-2 p-4">
       <ListNotes />
     </div>
     <main class="flex-1 p-4">
-      <div class="flex justify-between ">
-        <button @click="newNote">
-          <span>
-            <PlusCircleIcon class="h-6 w-6" />
-          </span>
-        </button>
-        <!--   <SearchBar />-->
-      </div>
       <RouterView />
     </main>
+  </div>
+  <div v-else>
+    <CardNotes />
   </div>
 </template>
